6e38ea4c02b5661fb1b2344992f6cd5534aa1e53
Cyrill <cyrillkuettel@gmail.com>
2023-03-20 00:42:52 +0100

    Add userscript

---
+++ overleaf_custom_vim_bindings/userscript.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Hide goodreads Buy on Amazon
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Hide goodreads "Buy on Amazon" button
+// @author       You
+// @match        https://www.goodreads.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @require      https://raw.githubusercontent.com/cyrillkuettel/userscripts/master/hide_button_goodreads.js
+// @grant        none
+// @require      https://raw.githubusercontent.com/cyrillkuettel/userscripts/master/overleaf_custom_vim_bindings/overleaf_custom_vim_bindings.js
+// ==/UserScript==
+
