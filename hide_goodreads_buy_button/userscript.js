81288bcbf308fe389660e8d61c491a5ee594fb2e
Cyrill <cyrillkuettel@gmail.com>
2023-03-20 00:37:55 +0100

    Add structure

---
+++ hide_goodreads_buy_button/userscript.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Hide goodreads Buy on Amazon
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Hide goodreads "Buy on Amazon" button
+// @author       Cyrill Küttel
+// @match        https://www.goodreads.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @require      https://raw.githubusercontent.com/cyrillkuettel/userscripts/master/hide_button_goodreads.js
+// @grant        none
+// ==/UserScript==
