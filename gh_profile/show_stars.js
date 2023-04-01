bf9e17522797fb3a9886db1b6da2b68fab38b850
Cyrill Küttel <cyrill.kuettel@seantis.ch>
2023-04-02 00:58:22 +0200

    Changes the order of things appearing on profile

---
+++ gh_profile/show_stars.js
@@ -0,0 +1,30 @@
+(function () {
+    'use strict';
+
+    if (document.readyState !== 'loading') {
+        main();
+    } else {
+        document.addEventListener('DOMContentLoaded', function () {
+            main();
+        });
+    }
+	
+    function main() {	
+      
+
+        (function () {
+                    
+          const parent = document.querySelector('turbo-frame#user-profile-frame').firstElementChild;
+          console.log(parent);
+          console.log(parent.children.length);
+
+          // swap
+          let tmp = parent.children[1];
+          parent.insertBefore(parent.children[2], tmp);
+          parent.insertBefore(tmp, parent.children[2].nextSibling);
+	
+        }());
+      
+    }
+})();
+
---
+++ gh_profile/userscript.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Github Profile styling
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Changes the order of things appearing on Github profile
+// @author       Cyrill Küttel
+// @match        https://github.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// @require      https://raw.githubusercontent.com/cyrillkuettel/userscripts/master/gh_profile/show_stars.js
+// ==/UserScript==
+
