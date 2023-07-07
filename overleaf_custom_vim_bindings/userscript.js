71e96d95a5397006d4e93d36702527c9acefd23e
cyrillkuettel <cyrillkuettel@gmail.com>
2023-07-07 19:23:01 +0200

    fix typo in url

--- overleaf_custom_vim_bindings/userscript.js
+++ overleaf_custom_vim_bindings/userscript.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  Overleaf custom vim bindings 
 // @author       Cyrill Küttel 
-// @match        https://www.goodreads.com/*
+// @match        https://www.overleaf.com/project/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // @require      https://raw.githubusercontent.com/cyrillkuettel/userscripts/master/overleaf_custom_vim_bindings/overleaf_custom_vim_bindings.js
