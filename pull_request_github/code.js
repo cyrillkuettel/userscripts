15e3f21d383d2a802a17f112d61b0d5094051efb
Cyrill Küttel <cyrill.kuettel@seantis.ch>
2023-10-21 18:57:16 +0200

    remove uneccessary top comment

--- pull_request_github/code.js
+++ pull_request_github/code.js
@@ -1,14 +1,3 @@
-// ==UserScript==
-// @name         Minimize .po files
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Minimize .po files in github pull request
-// @author       Cyrill Küttel
-// @match        https://github.com/*/*/pull/*/files
-// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @grant        none
-// ==/UserScript==
-
 (function () {
     'use strict';
 
